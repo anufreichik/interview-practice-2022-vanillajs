fae38fb8b8acf791391823eac090054dba82afdb
anufreichik <marta149@hotmail.com>
2022-09-17 10:59:52 -0700

    initial

---
+++ tree/tree.js
@@ -0,0 +1,104 @@
+const data = [
+    {
+        text: "JSON Node 1",
+        children: [
+            {
+                text: "JSON Node 1.1",
+                children:[
+                    {
+                        text: "JSON Node 1.1.1",
+                    },
+                    {
+                        text: "JSON Node 1.1.2",
+                    },
+                ]
+            },
+            {
+                text: "JSON Node 1.2",
+            },
+            {
+                text: "JSON Node 1.3",
+            },
+            {
+                text: "JSON Node 1.4",
+            },
+            {
+                text: "JSON Node 1.5",
+            },
+        ],
+    },
+    {
+        text: "JSON Node 2",
+        children: [
+            {
+                text: "JSON Node 2.1",
+            },
+            {
+                text: "JSON Node 2.2",
+            },
+            {
+                text: "JSON Node 2.3",
+            },
+            {
+                text: "JSON Node 2.4",
+                children:[
+                    {
+                        text: "JSON Node 2.4.1",
+                    },
+                    {
+                        text: "JSON Node 2.4.2",
+                    },
+                ]
+            },
+            {
+                text: "JSON Node 2.5",
+            },
+        ],
+    },
+]
+
+let container =document.querySelector(".container");
+
+//li.classList.add('li-styled');
+//li.style.marginTop='30px';
+
+let buildTree=(data, collapsable)=>{
+    const ul = document.createElement('ul');
+    if(collapsable) {
+        ul.classList.add('collapsed');
+    }
+    else{
+        ul.classList.add('expanded');
+    }
+    for(let el of data){
+        const li = document.createElement('li');
+        const elemText = document.createTextNode(el.text);
+
+        li.appendChild(elemText);
+
+        if(el.children && el.children.length){
+
+            let children = buildTree(el.children, true);
+
+
+            li.appendChild(children);
+            li.addEventListener('click',function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+                children.classList.toggle('collapsed');
+                console.log(e.target, 'clicked');
+            })
+        }
+
+        ul.appendChild(li);
+    }
+
+    return ul;
+
+}
+
+if(data.length){
+    let tree=buildTree(data,false)
+    container.appendChild(tree);
+}
+
