c028beaaaa95abdc3fb48425a99e8bab4657948f
anufreichik <marta149@hotmail.com>
2022-09-22 11:25:48 -0700

    star rating widget

--- starrating/starrating.js
+++ starrating/starrating.js
@@ -1,30 +1,46 @@
 const stars = document.querySelectorAll('.star');
 
-function selectStars(selectedStarID){
+function handleClickStar(selectedStarID){
     // let starsToSelect = document.querySelectorAll(`[data-id="${id}"]`);
-    stars.forEach(s=>{
-        let starId = s.getAttribute('data-id');
+    stars.forEach((s,ind)=>{
+        let starId = ind+1;
         if(Number(starId)<=Number(selectedStarID))
             s.classList.add('selected-star');
         else
             s.classList.remove('selected-star');
     })
 }
-stars.forEach(el=>{
+
+function handleHoverStar(selectedStarID){
+    stars.forEach((s,ind)=>{
+        //let starId = s.getAttribute('data-id');
+        let starId = ind+1;
+        if(Number(starId)<=Number(selectedStarID))
+            s.classList.add('hovered-star');
+        else
+            s.classList.remove('hovered-star');
+    })
+}
+
+
+stars.forEach((el, i)=>{
+    let selectedStarID = i+1;
+
     //click
     el.addEventListener('click', (e)=>{
-        let selectedStarID =  e.target.getAttribute('data-id');
-        selectStars(selectedStarID);
+        //let selectedStarID =  e.target.getAttribute('data-id');
+        handleClickStar(selectedStarID);
     })
 
     //hover
     el.addEventListener('mouseover',(e)=>{
-        let selectedStarID =  e.target.getAttribute('data-id');
-        selectStars(selectedStarID);
+        handleHoverStar(selectedStarID);
     })
 
     el.addEventListener('mouseout',(e)=>{
-
+        stars.forEach(s=>{
+            s.classList.remove('hovered-star');
+        })
     })
 
 })
